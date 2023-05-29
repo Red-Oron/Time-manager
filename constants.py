b540fd1be978177f59e87868c865c5546269050d
Миша <mishavveshnyakov@yandex.ru>
2023-05-29 15:54:17 +0300

    the data collection program is done

--- constants.py
+++ constants.py
@@ -1,12 +1,14 @@
 activities = {
-    0: 'sleep',
-    1: 'relax',
-    2: 'study'
+    1: ['sleep'],
+    2: ['relax', {1: 'watching video', 2: 'playing game', 3: 'other'}],
+    3: ['study', {1: 'lyceum', 2: 'schoolwork', 3: 'other'}],
+    4: ['other', {1: 'homework', 2: 'programming', 3: 'public transport', 4: 'other'}]
 }
 actions = {
     'q': 'finish step',
     'w': 'add description to step',
     'e': 'delete last step',
-    'r': 'finish program'
+    'r': 'go back',
+    't': 'finish program'
 }
-sp = ['q', 'w', 'e', 'r']
+sp = ['q', 'w', 'e', 'r', 't']
--- main.py
+++ main.py
@@ -6,35 +6,55 @@ run, step, name_step = True, False, ''
 
 def an(s):
     from save import steps
-    from constants import activities, actions, sp
+    from constants import activities
     global step, name_step, run
     if step:
         step = False
-    if s.isdigit():
+    elif s.isdigit():
         s = int(s)
-        step, name_step = True, activities[s - 1]
-        steps.append([activities[s - 1], dt.datetime.now().timestamp(), ''])
+        t = False
+        if s != 1:
+            print()
+            for j in range(1, len(activities[s][1]) + 1):
+                print(f'{j}: {activities[s][1][j]}')
+            print()
+            n = input()
+            while n != 'r' and int(n) not in activities[s][1]:
+                n = input()
+            if n != 'r':
+                name_step = f'{activities[s][0]}({activities[s][1][int(n)]})'
+            else:
+                print()
+                return
+        else:
+            name_step = activities[s][0]
+        step = True
+        steps.append([name_step, dt.datetime.now().timestamp(), ''])
         print(f'{name_step} step is underway')
+        s = input()
         while s != 'q':
-            s = input()
+            s = input('please enter "q"\n')
         an(s)
-    if s == 'w':
+    elif s == 'w':
         bar = input()
         steps[-1][-1] = bar
-    if s == 'e':
-        if input('y/n') == 'y':
+    elif s == 'e':
+        if input('y/n\n') == 'y':
             steps.pop(len(steps) - 1)
-    if s == 'r':
+    elif s == 't':
         run = False
+    else:
+        print()
 
 
 while run:
     print('choosing activity:')
-    for i in range(len(activities)):
-        print(f'{i + 1}: {activities[i]}')
+    for i in range(1, len(activities) + 1):
+        print(f'{i}: {activities[i][0]}')
     print()
     for i in range(len(sp)):
         print(f'{sp[i]}: {actions[sp[i]]}')
+    print()
     an(input())
 
 f = open('save.py', 'w')
